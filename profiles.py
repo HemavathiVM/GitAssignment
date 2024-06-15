1af4fe8c57c392e07f532e8d49cbb1f6c84f35db
aditya-nm <adityamuralidhar37@gmail.com>
2024-06-15 12:39:25 +0530

    Changes

--- profiles.py
+++ profiles.py
@@ -1,11 +1,17 @@
 def GetAdityaName():
     return "Aditya N M"
 
+def GetCharanName():
+    return "Sri Charan"
+
 while True:
     Argument= input()
     
     if Argument == "Aditya":
         print(GetAdityaName())
+    
+    if Argument == "Charan":
+        print(GetCharanName())
     
     elif Argument == "exit":
         break
