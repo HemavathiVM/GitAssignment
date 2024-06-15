b4c906215b630f34ca90cdeaf4b583a723f5260e
Abh1manyuSingh <165749976+Abh1manyuSingh@users.noreply.github.com>
2024-06-15 13:12:23 +0530

    abhi

--- profiles.py
+++ profiles.py
@@ -1,14 +1,21 @@
 def GetAdityaName():
     return "Aditya N M"
 
+def GetAbhimanyuName():
+    return "ABHIMANYU SINGH"
+
 while True:
     Argument= input()
     
     if Argument == "Aditya":
         print(GetAdityaName())
+
+    if Argument == "ABHIMANYU":
+        print(GetAbhimanyuName())
     
     elif Argument == "exit":
         break
+
     
     else:
         print("Invalid argument")
