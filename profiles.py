a72d442478ea1587a7fe4760cada379e52ff07a4
SuprabathReddyS <165735538+SuprabathReddyS@users.noreply.github.com>
2024-06-15 12:51:29 +0530

    Update profiles.py

--- profiles.py
+++ profiles.py
@@ -1,11 +1,16 @@
 def GetAdityaName():
     return "Aditya N M"
+def GetSuprabathName():
+    return "Surpabath reddy"
 
 while True:
     Argument= input()
     
     if Argument == "Aditya":
         print(GetAdityaName())
+
+    elif Argument =="Surpabath":
+        print(GetSuprabathName())
     
     elif Argument == "exit":
         break
