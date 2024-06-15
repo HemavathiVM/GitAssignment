6f1c349b36d5470cbfe77c5eb631802aef834e5b
vignesh <vigneshn032@gmail.com>
2024-06-15 13:09:33 +0530

    Update profiles.py

--- profiles.py
+++ profiles.py
@@ -5,7 +5,26 @@ while True:
     Argument= input()
     
     if Argument == "Aditya":
+        print(GetAdityaName())def GetAdityaName():
+    return "Aditya N M"
+def GetVigneshName():
+    return "Vignesh"
+
+while True:
+    Argument= input()
+    
+    if Argument == "Aditya":
         print(GetAdityaName())
+    
+    if Argument == "Vignesh":
+        print(GetVigneshName())
+    
+    elif Argument == "exit":
+        break
+    
+    else:
+        print("Invalid argument")
+
     
     elif Argument == "exit":
         break
