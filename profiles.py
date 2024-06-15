ffb3417a73aab425ae825c6d6ef0022491129f35
HemavathiVM <hemavathivm2004@gmali.com>
2024-06-15 13:02:03 +0530

    911

--- profiles.py
+++ profiles.py
@@ -1,11 +1,19 @@
 def GetAdityaName():
     return "Aditya N M"
+def GetBhoomiName():
+    return "Bhoomika R P"
+def GetHemaName():
+    return "Hemavathi V M"
 
 while True:
     Argument= input()
     
     if Argument == "Aditya":
         print(GetAdityaName())
+    if Argument == "Bhoomika":
+        print(GetBhoomiName())
+    if Argument == "Hemavathi":
+        print(GetHemaName())
     
     elif Argument == "exit":
         break
