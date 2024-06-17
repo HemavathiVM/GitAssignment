4848023ad09c37e90a6a3387ada6b0ba812d425d
AbhishekShirol <157870184+AbhishekShirol@users.noreply.github.com>
2024-06-17 10:55:47 +0530

    Update profiles.py

--- profiles.py
+++ profiles.py
@@ -1,14 +1,15 @@
 def GetAdityaName():
     return "Aditya N M"
-
+def GetAbhishekS():
+    return "AbhishekS"
 while True:
     Argument= input()
     
     if Argument == "Aditya":
         print(GetAdityaName())
-    
+    elif Argument == "AbhishekS"
+        print(GetAbhishekS())
     elif Argument == "exit":
-        break
-    
+        break 
     else:
         print("Invalid argument")
